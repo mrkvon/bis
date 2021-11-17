9556569d13ca08af634ccd0364b9404626dab781
mrkvon <mrkvon@protonmail.com>
2021-11-17 17:07:43 +0100

    Add react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { store } from './app/store'
-import { Provider } from 'react-redux'
+import './index.css'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>,
   document.getElementById('root'),
