c1e4a15453cd6e408f0fe4abe7889f29e137265d
mrkvon <mrkvon@users.noreply.github.com>
2021-12-08 10:16:18 +0100

    Add link from header to home (#8)
    Go to main page when clicking header logo

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,18 +1,21 @@
 import { useAppSelector } from './app/hooks'
 import { selectLogin } from './features/login/loginSlice'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   const { currentRole, isLoggedIn } = useAppSelector(selectLogin)
   return (
     <header className="pl-6 pr-6 h-16 flex items-center justify-between">
-      <h1 className="text-2xl font-extrabold">
-        Brontosaurus Panel{' '}
-        {currentRole === 'org'
-          ? 'Organizátora'
-          : currentRole === 'mem'
-          ? 'Člena'
-          : 'Uživatele'}
-      </h1>
+      <Link to="/">
+        <h1 className="text-2xl font-extrabold">
+          Brontosaurus Panel{' '}
+          {currentRole === 'org'
+            ? 'Organizátora'
+            : currentRole === 'mem'
+            ? 'Člena'
+            : 'Uživatele'}
+        </h1>
+      </Link>
       {isLoggedIn && <nav>Uživatel</nav>}
     </header>
   )
