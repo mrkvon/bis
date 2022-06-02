95ec23ecf736388672fbe3dd17bebee846ca078e
lamanchy <lamanchy@gmail.com>
2022-06-02 16:36:06 +0200

    donation permissions

--- backend/donations/admin.py
+++ backend/donations/admin.py
@@ -23,7 +23,7 @@ class DonationAdmin(NestedModelAdmin):
     def has_change_permission(self, request, obj=None): return False
 
     def has_delete_permission(self, request, obj=None):
-        return not obj or not obj.donor
+        return obj and not obj.donor
 
 
 class DonationAdminInline(NestedTabularInline):
