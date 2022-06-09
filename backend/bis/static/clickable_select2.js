b2684c3a33bdd18961650cceeb26f826987411a3
lamanchy <lamanchy@gmail.com>
2022-06-09 23:31:25 +0200

    fix clickable select

--- backend/bis/static/clickable_select2.js
+++ backend/bis/static/clickable_select2.js
@@ -1,7 +1,7 @@
 document.addEventListener("click", function(event){
     if (event.target.className === 'select2-selection__choice') {
         $el = event.target
-        index = Array.from($el.parentNode.childNodes).indexOf($el)
+        index = Array.from($el.parentNode.querySelectorAll('.select2-selection__choice')).indexOf($el)
         select = $el.parentElement.parentElement.parentElement.parentElement.parentElement.querySelector('select')
 
         id = select.querySelectorAll('option')[index].value
