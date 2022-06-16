aa668066fe20d89892d65ebd63f3b1af33be05d2
lamanchy <lamanchy@gmail.com>
2022-06-16 15:08:22 +0200

    opportunities admin list

--- backend/opportunities/admin.py
+++ backend/opportunities/admin.py
@@ -1,4 +1,5 @@
 from nested_admin.nested import NestedModelAdmin
+from rangefilter.filters import DateRangeFilter
 
 from bis.admin_helpers import EditableByAdminOnlyMixin
 from event.models import *
@@ -7,9 +8,14 @@ from opportunities.models import Opportunity, OfferedHelp
 
 @admin.register(Opportunity)
 class OpportunityAdmin(EditableByAdminOnlyMixin, NestedModelAdmin):
+    list_display = 'name', 'category', 'contact_person', 'start', 'end', 'on_web_start', 'on_web_end', 'location'
     autocomplete_fields = 'location', 'contact_person'
     save_as = True
-    pass
+
+    list_select_related = 'category', 'contact_person'
+    list_filter = 'category', ('start', DateRangeFilter), ('end', DateRangeFilter),
+
+    search_fields = 'name', 'introduction'
 
 
 @admin.register(OfferedHelp)
