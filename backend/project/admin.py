b58d21ac7de7a59a8e18de2292fab09a4cfdbfd1
lamanchy <lamanchy@gmail.com>
2022-06-17 12:49:36 +0200

    styling

--- backend/project/admin.py
+++ backend/project/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 
 class MyAdminSite(admin.AdminSite):
     site_title = 'BIS'
-    site_header = 'BIS administrace'
+    site_header = mark_safe(
+        '<img src="/backend_static/logo/br_white_right.png" style="height: 60px; margin: -5px 0px -5px -35px">'
+        'BIS administrace')
     index_title = 'Ať žije nový BIS!'
     site_url = 'https://brontosaurus.cz/'
 
@@ -19,4 +23,3 @@ class MyAdminSite(admin.AdminSite):
         ]
         list.sort(key=lambda value: order.index(value['app_label']))
         return list
-
