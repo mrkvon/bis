3be63d5ef1d7641bfd8f51a8e7b6a2ab51a2f569
lamanchy <lamanchy@gmail.com>
2022-07-26 11:05:06 +0200

    categories not editable (because of slug)

--- backend/categories/admin.py
+++ backend/categories/admin.py
@@ -1,64 +1,64 @@
 from django.contrib import admin
 
-from bis.admin_helpers import EditableByAdminOnlyMixin
+from bis.admin_helpers import ReadOnlyMixin
 from categories.models import *
 
 
 @admin.register(GrantCategory)
-class GrantCategoryAdmin(EditableByAdminOnlyMixin, admin.ModelAdmin):
+class GrantCategoryAdmin(ReadOnlyMixin, admin.ModelAdmin):
     pass
 
 
 @admin.register(PropagationIntendedForCategory)
-class PropagationIntendedForCategoryAdmin(EditableByAdminOnlyMixin, admin.ModelAdmin):
+class PropagationIntendedForCategoryAdmin(ReadOnlyMixin, admin.ModelAdmin):
     pass
 
 
 @admin.register(DietCategory)
-class DietCategoryAdmin(EditableByAdminOnlyMixin, admin.ModelAdmin):
+class DietCategoryAdmin(ReadOnlyMixin, admin.ModelAdmin):
     pass
 
 
 @admin.register(QualificationCategory)
-class QualificationCategoryAdmin(EditableByAdminOnlyMixin, admin.ModelAdmin):
+class QualificationCategoryAdmin(ReadOnlyMixin, admin.ModelAdmin):
     list_display = 'slug', 'name', 'parent'
 
 
 @admin.register(AdministrationUnitCategory)
-class AdministrationUnitCategoryAdmin(EditableByAdminOnlyMixin, admin.ModelAdmin):
+class AdministrationUnitCategoryAdmin(ReadOnlyMixin, admin.ModelAdmin):
     pass
 
 
 @admin.register(MembershipCategory)
-class MembershipCategoryAdmin(EditableByAdminOnlyMixin, admin.ModelAdmin):
+class MembershipCategoryAdmin(ReadOnlyMixin, admin.ModelAdmin):
     pass
 
 
 @admin.register(EventCategory)
-class EventCategoryAdmin(EditableByAdminOnlyMixin, admin.ModelAdmin):
+class EventCategoryAdmin(ReadOnlyMixin, admin.ModelAdmin):
     pass
 
 
 @admin.register(EventProgramCategory)
-class EventProgramCategoryAdmin(EditableByAdminOnlyMixin, admin.ModelAdmin):
+class EventProgramCategoryAdmin(ReadOnlyMixin, admin.ModelAdmin):
     pass
 
 
 @admin.register(DonationSourceCategory)
-class DonationSourceCategoryAdmin(EditableByAdminOnlyMixin, admin.ModelAdmin):
+class DonationSourceCategoryAdmin(ReadOnlyMixin, admin.ModelAdmin):
     pass
 
 
 @admin.register(OrganizerRoleCategory)
-class OrganizerRoleCategoryAdmin(EditableByAdminOnlyMixin, admin.ModelAdmin):
+class OrganizerRoleCategoryAdmin(ReadOnlyMixin, admin.ModelAdmin):
     pass
 
 
 @admin.register(TeamRoleCategory)
-class TeamRoleCategoryAdmin(EditableByAdminOnlyMixin, admin.ModelAdmin):
+class TeamRoleCategoryAdmin(ReadOnlyMixin, admin.ModelAdmin):
     pass
 
 
 @admin.register(OpportunityCategory)
-class OpportunityCategoryAdmin(EditableByAdminOnlyMixin, admin.ModelAdmin):
+class OpportunityCategoryAdmin(ReadOnlyMixin, admin.ModelAdmin):
     pass
