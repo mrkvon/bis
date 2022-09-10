e0ff83a8e241acb443b8701e09bb2287af973e5b
lamanchy <lamanchy@gmail.com>
2022-09-10 22:45:47 +0200

    rm frontend files

--- frontend/src/features/login/types.ts
+++
@@ -1,8 +0,0 @@
-export type Credentials = { username: string; password: string }
-
-export type Role = keyof typeof existingRoles
-
-export const existingRoles = {
-  org: 'Organizátor',
-  mem: 'Člen',
-}
