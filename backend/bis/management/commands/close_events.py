305f49bed5e38790acec2791f93cccc17bca869f
lamanchy <lamanchy@gmail.com>
2022-10-08 09:33:06 +0200

    skip validation when closing events

--- backend/bis/management/commands/close_events.py
+++ backend/bis/management/commands/close_events.py
@@ -4,10 +4,12 @@ from dateutil.utils import today
 from django.core.management.base import BaseCommand
 
 from bis.emails import email_event_closed
+from bis.helpers import with_paused_validation
 from event.models import Event
 
 
 class Command(BaseCommand):
+    @with_paused_validation
     def handle(self, *args, **options):
         for event in Event.objects.filter(end__lt=today().date() - timedelta(days=20), is_closed=False):
             event.is_closed = True
