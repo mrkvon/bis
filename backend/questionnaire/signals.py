3da5a06e9387bd530207da35a01a2b2de8e5dc7d
Ondřej Lomič <ondrej.lomic@gmail.com>
2022-12-04 10:29:14 +0100

    set event application user

--- backend/questionnaire/signals.py
+++ backend/questionnaire/signals.py
@@ -1,3 +1,14 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
 from questionnaire.models import *
+
+
+@receiver(pre_save, sender=EventApplication, dispatch_uid='set_event_application_user')
+def set_event_application_user(instance: EventApplication, **kwargs):
+    instance.user = (
+            instance.birthday and User.objects.filter(first_name=instance.first_name,
+                                                      last_name=instance.last_name,
+                                                      birthday=instance.birthday).first() or
+            instance.email and User.objects.filter(all_emails__email=instance.email).first()
+    )
